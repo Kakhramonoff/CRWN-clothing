7610b06a0655a4ed79de57310fea68dedf97a977
Qahramonov <qahramonovforbootcamp@gmail.com>
2023-08-31 14:35:54 +0500

    Sign in page is ended, added shop data json

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Home from './pages/Home/Home';
 import Navigation from './routes/Navigation';
 import Auth from './pages/Auth/Auth';
+import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
         <Route path='auth' element={<Auth />}/>
-        {/* <Route path='contact' element={<Contact />}/> */}
       </Route>
     </Routes>
   );
